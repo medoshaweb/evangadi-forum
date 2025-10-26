a8b4e0cd26bb3043cac55499a95fa090eafb0a17
medoshaweb <hailugs@yahoo.com>
2025-10-26 12:39:59 -0700

    frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// ✅ use environment variable
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-
 const API = axios.create({
   baseURL: "http://localhost:5000/api",
   headers: {
