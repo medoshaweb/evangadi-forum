ed2e6f0fdc690a31384d74951123ef9369ae97f3
medoshaweb <hailugs@yahoo.com>
2025-10-28 10:31:12 -0700

    vite config

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ import userRoutes from "./routes/userRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://me-dosha.com", credentials: true }));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
